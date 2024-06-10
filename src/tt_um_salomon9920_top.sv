1c82b7e5a8f2727c4eecad4f69426b69509d00a9
5salomon <162090484+5salomon@users.noreply.github.com>
2024-06-09 22:19:07 -0600

    s

---
+++ src/tt_um_salomon9920_top.sv
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2024 11:37:32
+// Design Name: 
+// Module Name: tt_um_salomon9920_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tt_um_salomon9920_top(
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+    );
+    
+    wire tx;
+    wire sda;
+    
+    top_module top(
+    .clk(clk),
+    .reset(rst_n),
+    .tx(tx),
+    .scl(uio_out),
+    .sda(sda) 
+    );
+   
+endmodule
